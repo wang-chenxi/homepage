7f137b7902128c2a3a96510a85430ba4d7ec90a0
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-07-02 01:17:27 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var profilePhoto = document.querySelector(".headpage img");
+
+function profileOn() {
+    profilePhoto.src = "./assets/profile-1.JPG"
+}
+
+function profileOver() {
+    profilePhoto.src = "./assets/profile-2.JPG"
+}
+
+// function scale(element) {
+//     element.style.transform = "scale(1.5)";
+//     element.transition = "500ms";
+// }
