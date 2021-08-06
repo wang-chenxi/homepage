248922e80dbd5ebc23b6bc86210970f1a28a32c3
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-08-05 18:04:39 -0700

    8/5/2021

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var profilePhoto = document.querySelector(".headpage img");
 
-function profileOn() {
-    profilePhoto.src = "./assets/profile-1.JPG"
-}
+// function profileOn() {
+//     profilePhoto.src = "./assets/profile-1.JPG"
+// }
 
-function profileOver() {
-    profilePhoto.src = "./assets/profile-2.JPG"
-}
+// function profileOver() {
+//     profilePhoto.src = "./assets/profile-2.JPG"
+// }
